3f1093e47717024c5e68873aa9556b82fc52026d
johntheo <john.theo.souza@gmail.com>
2016-11-23 06:07:02 -0200

    Alteração de identidade
    REFACTOR.
    modificado o nome da aplicação, alterado os ícones

--- web/public/firebase-messaging-sw.js
+++ web/public/firebase-messaging-sw.js
@@ -4,8 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: '670781077200'
+  messagingSenderId: '877514274517'
 });
 const messaging = firebase.messaging();
-
-
